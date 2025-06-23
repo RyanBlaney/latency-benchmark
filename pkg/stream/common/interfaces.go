b9e4a4d83105b75560326e45ec558c2d1fb542ad
Ryan Blaney <ryanblaney2004@gmail.com>
2025-06-23 14:25:21 -0700

    Updated interface between stream types

--- pkg/stream/common/interfaces.go
+++ pkg/stream/common/interfaces.go
@@ -112,37 +112,3 @@ type StreamValidator interface {
 	// ValidateAudio validates audio data quality
 	ValidateAudio(data *AudioData) error
 }
-
-// StreamError represents stream-related errors
-type StreamError struct {
-	Type    StreamType `json:"type"`
-	URL     string     `json:"url"`
-	Code    string     `json:"code"`
-	Message string     `json:"message"`
-	Cause   error      `json:"-"`
-}
-
-func (e *StreamError) Unwrap() error {
-	return e.Cause
-}
-
-// Common error codes
-const (
-	ErrCodeConnection    = "CONNECTION_FAILEeD"
-	ErrCodeTimeout       = "TIMEOUT"
-	ErrCodeInvalidFormat = "INVALID_FORMAT"
-	ErrCodeDecoding      = "DECODING_FAILED"
-	ErrCodeMetadata      = "METADATA_ERROR"
-	ErrCodeUnsupported   = "UNSUPPORTED_STREAM"
-)
-
-// NewStreamError creates a new stream error
-func NewStreamError(streamType StreamType, url, code, message string, cause error) *StreamError {
-	return &StreamError{
-		Type:    streamType,
-		URL:     url,
-		Code:    code,
-		Message: message,
-		Cause:   cause,
-	}
-}
